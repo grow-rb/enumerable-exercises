169053849ae37148cc5a19310732f5a3a8617c8d
okuramasafumi <masafumi.o1988@gmail.com>
2019-04-03 19:16:41 +0900

    Add test and README for `map`

---
+++ map/README.md
@@ -0,0 +1,4 @@
+# Todo
+
+Create a file named `map.rb`.
+Open `Array` class, undefine `map` method and define your own only using `each`.
---
+++ map/map_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require_relative 'map'
+
+class MapTest < MiniTest::Test
+  def test_with_block
+    assert_equal [1, 2, 3].map {|i| i ** 2 }, [1, 4, 9]
+  end
+
+  def test_with_symbol_to_proc
+    assert_equal [1, 2, 3].map(&:to_s), ['1', '2', '3']
+  end
+end
