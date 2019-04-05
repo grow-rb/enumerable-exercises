6389b2c0c1109b64c4703548106d295e36fb7c15
okuramasafumi <masafumi.o1988@gmail.com>
2019-04-05 17:19:59 +0900

    Add README and tests for `inject`

---
+++ inject/README.md
@@ -0,0 +1,5 @@
+# TODO
+
+Create a file named `inject.rb`.
+Open a module `Enumerable` and undefine `inject` method.
+Define your own `inject` method to pass the tests.
---
+++ inject/inject_test.rb
@@ -0,0 +1,24 @@
+require 'minitest/autorun'
+require_relative 'inject'
+
+class InjectTest < Minitest::Test
+  def test_sum_some_numbers
+    assert_equal 45, (5..10).inject(:+)
+  end
+
+  def test_sum_numbers_with_block
+    assert_equal 45, (5..10).inject {|sum, n| sum + n }
+  end
+
+  def test_multiple_some_numbers
+    assert_equal 151200, (5..10).inject(1, :*)
+  end
+
+  def test_multiple_some_numbers_with_block
+    assert_equal 151200, (5..10).inject(1) {|product, n| product * n}
+  end
+
+  def test_finding_the_longest_word
+    assert_equal 'sheep', %w(cat sheep bear).inject {|memo, word| memo.length > word.length ? memo : word}
+  end
+end
