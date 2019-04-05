a21ffa61b00388a2dd3bf10e3de586e9f53ae7c1
Akihito Tsukamoto <tsuka05@gmail.com>
2019-04-05 20:10:49 +0900

    Fix argument order

--- map/map_test.rb
+++ map/map_test.rb
@@ -3,10 +3,10 @@ require_relative 'map'
 
 class MapTest < MiniTest::Test
   def test_with_block
-    assert_equal [1, 2, 3].map {|i| i ** 2 }, [1, 4, 9]
+    assert_equal [1, 4, 9], [1, 2, 3].map {|i| i ** 2 }
   end
 
   def test_with_symbol_to_proc
-    assert_equal [1, 2, 3].map(&:to_s), ['1', '2', '3']
+    assert_equal ['1', '2', '3'], [1, 2, 3].map(&:to_s)
   end
 end
