60aacb787a850aa62c7153c612830bbc98dedb0d
okuramasafumi <masafumi.o1988@gmail.com>
2019-04-06 16:41:09 +0900

    Add range test for `map` method

--- map/map_test.rb
+++ map/map_test.rb
@@ -9,4 +9,8 @@ class MapTest < MiniTest::Test
   def test_with_symbol_to_proc
     assert_equal [1, 2, 3].map(&:to_s), ['1', '2', '3']
   end
+
+  def test_for_range
+    assert_equal [1, 4, 9], (1..3).map {|i| i ** 2 }
+  end
 end
