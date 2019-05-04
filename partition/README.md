eb34e83a5da54556ad3f7b434857d2308d0d7ea5
okuramasafumi <masafumi.o1988@gmail.com>
2019-05-04 20:57:35 +0900

    Add README and tests for `partition`

---
+++ partition/README.md
@@ -0,0 +1,5 @@
+# TODO
+
+Create a file named `partition.rb`.
+Open a module `Enumerable` and undefine `partition` method.
+Define your own `partition` method to pass the tests.
---
+++ partition/partition_test.rb
@@ -0,0 +1,8 @@
+require 'minitest/autorun'
+require_relative 'partition'
+
+class PartitionTest < MiniTest::Test
+  def test_it_works_with_even_odd
+    assert_equal [[2, 4, 6], [1, 3, 5]], (1..6).partition { |v| v.even? }
+  end
+end
