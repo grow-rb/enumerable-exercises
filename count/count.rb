3aa701d58029e7705aa441bf34507a1c768ff1e2
Shu OGAWARA <expajp@gmail.com>
2019-05-18 18:26:43 +0900

    Arrayへの定義をDRYに

--- count/count.rb
+++ count/count.rb
@@ -11,15 +11,5 @@ module Enumerable
   end
 end
 
-class Array
-  def count(*values, &block)
-    raise StandardError if values.length > 1
-    item = values.first unless values.empty?
-
-    ret = 0
-    self.each do |n|
-      ret += 1 if (values.empty? && !block_given?) || block_given? && yield(n) || !values.empty? && item == n
-    end
-    ret
-  end
-end
+# Arrayにもcountを定義
+Array.class_eval{ define_method(:count, Enumerable.instance_method(:count)) }
--- uniq/uniq.rb
+++ uniq/uniq.rb
@@ -13,17 +13,5 @@ module Enumerable
   end
 end
 
-class Array
-  def uniq
-    ret = []
-    checkers = []
-    self.each do |n|
-      checker = block_given? ? yield(n) : n
-      unless checkers.include?(checker)
-        ret << n
-        checkers << checker
-      end
-    end
-    ret
-  end
-end
+# Arrayにもuniqを定義
+Array.class_eval{ define_method(:uniq, Enumerable.instance_method(:uniq)) }
