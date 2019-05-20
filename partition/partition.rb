a638739d5e415c9823b66fa3eb72298ad9e11b1d
kzkick2nd <aruga.kazuki@gmail.com>
2019-05-20 23:43:54 +0900

    Pass test partition()

--- partition/partition.rb
+++ partition/partition.rb
@@ -1,5 +1,14 @@
 module Enumerable
     def partition
-        return [], []
+        t = []
+        f = []
+        self.each do |v|
+            if yield(v) then
+                t.push(v)
+            else
+                f.push(v)
+            end
+        end
+        return t, f
     end
 end
