dd3f88cf8078949818032dbfe998df73d124e21a
kzkick2nd <aruga.kazuki@gmail.com>
2019-05-20 23:37:09 +0900

    Pass test count()

--- count/count.rb
+++ count/count.rb
@@ -1,11 +1,31 @@
 module Enumerable
     def count(a = nil)
-        return 0
+        c = 0
+        self.each do |v|
+            if block_given? then
+                c += 1 if yield(v)
+            elsif a != nil && v == a then
+                c += 1
+            elsif a == nil then
+                c += 1
+            end
+        end
+        return c
     end
 end
 
 class Array
     def count(a = nil)
-        return 0
+        c = 0
+        self.each do |v|
+            if block_given? then
+                c += 1 if yield(v)
+            elsif a != nil && v == a then
+                c += 1
+            elsif a == nil then
+                c += 1
+            end
+        end
+        return c
     end
 end
